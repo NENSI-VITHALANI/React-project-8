9974f4df24a62c17caa47ac90e899f2bb3426df8
NENSI-VITHALANI <nensivithalani2484@gmail.com>
2023-09-11 00:46:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
+
 import logo from './logo.svg';
 import './App.css';
+import record from './Data';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [search, setSearch] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    let filteredByCategory = selectedCategory === 'All' ? record : record.filter(item => item.category === selectedCategory);
+
+    let filteredBySearch = filteredByCategory.filter(item =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setFilteredData(filteredBySearch);
+  }, [selectedCategory, search]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <body>
+        <h1 className='text-center text-white py-5 mb-5 border-bottom'>Welcome to My E-library</h1>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-3 d-flex flex-column'>
+              <button className='py-3 w-75 btn-secondary btn  my-3 text-black ' onClick={() => setSelectedCategory('All')} >All</button>
+              <button className='py-3 w-75 btn-secondary btn  my-3 text-black ' onClick={() => setSelectedCategory('Adventure')}>Adventure</button>
+              <button className='py-3 w-75 btn-secondary btn   my-3 text-black ' onClick={() => setSelectedCategory('Comic Book')}>Comic Book</button>
+              <button className='py-3 w-75 btn-secondary btn   my-3 text-black ' onClick={() => setSelectedCategory('Detective Book')}>Detective Book</button>
+              <button className='py-3 w-75 btn-secondary btn  my-3 text-black ' onClick={() => setSelectedCategory('Fantasy Book')}>Fantasy Book</button>
+              <button className='py-3 w-75 btn-secondary btn   my-3 text-black ' onClick={() => setSelectedCategory('Historical Fiction')}>Historical Fiction</button>
+              <button className='py-3 w-75 btn-secondary btn   my-3 text-black ' onClick={() => setSelectedCategory('Suspense and Thrillers')}>Suspense and Thrillers</button>
+              <button className='py-3 w-75 btn-secondary btn   my-3 text-black ' onClick={() => setSelectedCategory('Short Stories')}>Short Stories</button>
+              <button className='py-3 w-75 btn-secondary btn   my-3 text-black ' onClick={() => setSelectedCategory('Horror book')}>Horror book</button>
+              <button className='py-3 w-75 btn-secondary btn  my-3 text-black ' onClick={() => setSelectedCategory('Science Fiction book')}> Science Fiction book</button>
+            </div>
+            <div className='col-9 overflow-hidden'>
+              <table className='d-flex justify-content-center '>
+                <tr className='w-75 mt-3 mb-4 bg-light btn  d-flex justify-content-center text-secondary'>
+                  <td className='text-center'><input type="text" className='text-center btn py-2 px-5 fs-4 fw-bold' onChange={(e) => setSearch(e.target.value)} placeholder="Search Your Book" /></td>
+                </tr>
+              </table>
+          
+              <div className='row '>
+                {filteredData.map((item, index) => (
+                  <div key={index} className='col-4'>
+                    <div className='book-card my-3'>
+                      <img src={item.image} alt={item.name} />
+                      <h4 className='text-center my-2 p-3'>{item.name}</h4>
+                      <h5 className='text-center py-3 '>{item.category}</h5>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div >
+
+
+
+
+
+
+      </body >
+
+    </>
   );
 }
+
 
 export default App;
---
+++ src/Data.jsx
@@ -0,0 +1,175 @@
+const record = [
+    {
+        Id: 1,
+        name: 'Life of Pi',
+        image:'./images/1572292455-51rxEvLljUL.jpg',
+        category: 'Adventure'
+    },
+    {
+        Id: 2,
+        name: 'The Three Musketeers',
+        image: './images/1572292481-41TxXqToCCL.jpg',
+        category: 'Adventure'
+    },
+    {
+        Id: 3,
+        name: 'The Call of the Wild',
+        image: './images/1572292506-41A0yff934L.jpg',
+        category: 'Adventure'
+    },
+
+    {
+        Id: 4,
+        name: 'Watchmen',
+        image: './images/1572292885-41ZJD5Dw5KL.jpg',
+        category: 'Comic Book'
+    },
+    {
+        Id: 5,
+        name: 'The Walking Dead: Compendium One',
+        image: './images/1572292913-51S12ntkIbL.jpg',
+        category: 'Comic Book'
+    },
+    {
+        Id: 6,
+        name: 'The Boy, the Mole, the Fox and the Horse',
+        image: './images/1572292943-418D9yYGB3L.jpg',
+        category: 'Comic Book'
+    },
+
+    {
+        Id: 7,
+        name: 'The Night Fire',
+        image: './images/1572293115-41zqZwXZ-WL.jpg',
+        category: 'Detective Book'
+    },
+    {
+        Id: 8,
+        name: 'The Adventures of Sherlock Holmes',
+        image: './images/1572293468-516dZVA7rQL.jpg',
+        category: 'Detective Book'
+    },
+    {
+        Id: 9,
+        name: 'And Then There Were None',
+        image: './images/1572293489-51MlxNgCsyL.jpg',
+        category: 'Detective Book'
+    },
+
+    {
+        Id: 10,
+        name: 'The Water Dancer',
+        image: './images/1572293716-51ItgiZw5dL.jpg',
+        category: 'Fantasy Book'
+    },
+    {
+        Id: 11,
+        name: 'Circe',
+        image: './images/1572293746-51dzuwLmm-L.jpg',
+        category: 'Fantasy Book'
+    },
+    {
+        Id: 12,
+        name: 'Ninth House',
+        image: './images/1572293766-41cUxNtr5-L.jpg',
+        category: 'Fantasy Book'
+    },
+
+    {
+        Id: 13,
+        name: 'The Help',
+        image: './images/1572293965-51D9LgMgL.jpg',
+        category: 'Historical Fiction'
+    },
+    {
+        Id: 14,
+        name: 'One Hundred Years of Solitude',
+        image: './images/1572294012-51Zr6Pd32ML.jpg',
+        category: 'Historical Fiction'
+    },
+    {
+        Id: 15,
+        name: 'Vintage Memoirs of a Geisha',
+        image: './images/1572294103-51O9HqW8ZAL.jpg',
+        category: 'Historical Fiction'
+    },
+
+    {
+        Id: 16,
+        name: 'Gone Girl',
+        image: './images/1572297615-416TH3igfOL.jpg',
+        category: 'Suspense and Thrillers'
+    },
+    {
+        Id: 17,
+        name: 'The 19th Christmas',
+        image: './images/1572297634-51wT1Ny84sL.jpg',
+        category: 'Suspense and Thrillers'
+    },
+    {
+        Id: 18,
+        name: 'The Guardians',
+        image: './images/1572297648-51U2hIUQgqL.jpg',
+        category: 'Suspense and Thrillers'
+    },
+
+    {
+        Id: 19,
+        name: 'This Is How You Lose Her',
+        image: './images/1572297115-51G2GGjeBjL.jpg',
+        category: 'Short Stories'
+    },
+    {
+        Id: 20,
+        name: 'Florida',
+        image: './images/1572297319-51Y82I5ACdL.jpg',
+        category: 'Short Stories'
+    },
+    {
+        Id: 21,
+        name: 'How Long til Black Future Month?',
+        image: './images/1572297335-51XhaIdxkFL.jpg',
+        category: 'Short Stories'
+    },
+
+    {
+        Id: 22,
+        name: 'Carrie',
+        image: './images/1572295234-51dRjo8NJeL (1).jpg',
+        category: 'Horror book'
+    },
+    {
+        Id: 23,
+        name: 'The Haunting of Hill House',
+        image: './images/1572295266-51gyXcg1P2L.jpg',
+        category: 'Horror book'
+    },
+    {
+        Id: 24,
+        name: 'Bird Box',
+        image: './images/1572295284-41sRIihnXzL.jpg',
+        category: 'Horror book'
+    },
+
+    {
+        Id: 25,
+        name: 'Nan A. Talese The Testaments',
+        image: './images/1572296624-419TWZk7luL.jpg',
+        category: 'Science Fiction book'
+    },
+    {
+        Id: 26,
+        name: 'The Hunger Games Trilogy',
+        image: './images/1572296661-41nphb8vbPL.jpg',
+        category: 'Science Fiction book'
+    },
+    {
+        Id: 27,
+        name: '1984',
+        image: './images/1572296688-410ZirPKXKL.jpg',
+        category: 'Science Fiction book'
+    },
+]
+
+
+export default record
